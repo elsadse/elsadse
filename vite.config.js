4b6fdd0c972137e71f94f78ec21a9799577d4895
ElsaDse <elsasifoua02@gmail.com>
2026-03-02 07:12:59 +0100

    Finalisation de l'interface responsive.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import posthtml from '@vituum/vite-plugin-posthtml'
 import expressions from 'posthtml-expressions'
 
 export default defineConfig({
+  base:"/elsadse/",
   plugins: [
-    tailwindcss(),  // ← le plugin officiel qui gère tout
-
+    tailwindcss(),  
     posthtml({
       root: './src',
       plugins: [
@@ -15,10 +14,9 @@ export default defineConfig({
       ]
     })
   ],
-
-  root: 'src',              // ton index.html est dans src/
+  root: 'src',              
   build: {
-    outDir: '../dist',      // build final dans dist/ à la racine
+    outDir: '../dist',      
     emptyOutDir: true,
   },
 })
